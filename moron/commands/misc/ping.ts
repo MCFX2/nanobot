7e5711767c188f3edeffcb34752d8e41a569e968
rzl <mcfx2@mcfx2.co>
2025-02-11 08:02:33 -0600

    update ping cmd

--- moron/commands/misc/ping.ts
+++ moron/commands/misc/ping.ts
@@ -8,7 +8,17 @@ module.exports = {
 		.setName("ping")
 		.setDescription("Ping the bot"),
 	async execute(interaction: ChatInputCommandInteraction) {
-		interaction.reply("you are giving me a headache");
+		const replies = [
+			"you are giving me a headache",
+			"ouch",
+			"pong",
+			"stop that",
+			"why",
+			"no",
+			"GAH",
+			"I WAS SLEEPING ASSHOLE",
+		];
+		interaction.reply(replies[Math.floor(Math.random() * replies.length)]);
 	},
 	disable: false,
 };
